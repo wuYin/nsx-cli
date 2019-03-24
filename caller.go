9d21551621d19ad3a6178cc1849c356b7d616e43
wuYin <wuYinPost@gmail.com>
2019-03-24 12:22:53 +0800

    调用参数使用 JSON 序列化后再传递值

--- caller.go
+++ caller.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"encoding/json"
-	"fmt"
 	"nix/codec"
 	"nix/service"
 	"reflect"
@@ -10,13 +9,13 @@ import (
 	"tron"
 )
 
-type Consumer struct {
+type Caller struct {
 	uri2Service   map[string]service.Service
 	clientManager *NixClientManager
 }
 
-func NewConsumer(services []service.Service) *Consumer {
-	c := &Consumer{uri2Service: make(map[string]service.Service)}
+func NewCaller(services []service.Service) *Caller {
+	c := &Caller{uri2Service: make(map[string]service.Service)}
 
 	var uris []string
 	for _, s := range services {
@@ -32,12 +31,12 @@ func NewConsumer(services []service.Service) *Consumer {
 	return c
 }
 
-func (c *Consumer) GetService(uri string) interface{} {
+func (c *Caller) GetService(uri string) interface{} {
 	return c.uri2Service[uri].Instance
 }
 
 // 封装目标方法与网络调用
-func (c Consumer) makeRPCFunc(s service.Service) {
+func (c *Caller) makeRPCFunc(s service.Service) {
 	p := reflect.ValueOf(s.Instance).Elem() // Instance 其实是 Proxy
 	pt := reflect.TypeOf(p.Interface())
 	for i := 0; i < p.NumField(); i++ { // 全部是值为 nil 的方法字段
@@ -57,7 +56,7 @@ func (c Consumer) makeRPCFunc(s service.Service) {
 }
 
 // 请求网络发起调用并返回
-func (c Consumer) call(uri string, method string, mType reflect.Type, in []reflect.Value) []reflect.Value {
+func (c *Caller) call(uri string, method string, mType reflect.Type, in []reflect.Value) []reflect.Value {
 	// 1. 选取已连接到指定服务的 client
 	cli, err := c.clientManager.SelectClient(uri)
 	if err != nil {
@@ -92,10 +91,10 @@ func (c Consumer) call(uri string, method string, mType reflect.Type, in []refle
 		panic("call: unmarshal failed:" + err.Error())
 	}
 
-	respV := reflect.ValueOf(callResp.Res)
-	if !respV.Type().ConvertibleTo(mType.Out(0)) {
-		panic(fmt.Sprintf("can't convert resp %+v to %+v", respV.Type(), mType.Out(0)))
+	zeroVPtr := reflect.New(mType.Out(0))
+	if err = json.Unmarshal(callResp.Res, zeroVPtr.Interface()); err != nil {
+		panic("call: unmarshal: failed:" + err.Error())
 	}
 
-	return []reflect.Value{respV.Convert(mType.Out(0))}
+	return []reflect.Value{zeroVPtr.Elem()}
 }
